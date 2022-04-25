59faeb43971cae95b0642e611d31321222243b32
AntreasAntoniou <a.antoniou@ed.ac.uk>
2022-04-25 12:24:07 +0100

    Debugging

--- src/datasets/loader.py
+++ src/datasets/loader.py
@@ -75,7 +75,7 @@ def get_dataloader(
             episode_descr_config=episod_config,
         )
 
-        pprint(int(batch_size / world_size))
+        pprint(f"Batch size per worker {int(batch_size / world_size)}")
         worker_init_fn = partial(worker_init_fn_, seed=args.seed)
         data_loader = DataLoader(
             dataset=dataset,
