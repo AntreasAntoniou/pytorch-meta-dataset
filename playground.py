5a601dae5ef4a7b21bacf329a1075214488cf94c
AntreasAntoniou <a.antoniou@ed.ac.uk>
2022-04-25 04:36:26 +0100

    Update minimal playground for testing

--- playground.py
+++ playground.py
@@ -1,9 +1,17 @@
 import torch
-
 from src.models.standard.tali import TALIMP, default_cfgs
+from rich import print
 
 model = TALIMP(**default_cfgs["modus_prime_tali_viat_pretrained"])
 x_image = torch.randn(2, 3, 288, 176)
-x_text = torch.randn(2, 77)
+x_text = torch.randint(high=100, size=(2, 77))
 
-out = model.forward(x_image, x_text)
+out_image = model.forward(x_image)
+
+print(f"Test with {x_image.shape} and {x_text.shape} -> {out_image.shape}")
+
+out_image, out_text = model.forward(x_image=x_image, x_text=x_text)
+
+print(
+    f"Test with {x_image.shape} and {x_text.shape} -> {out_image.shape} {out_text.shape}"
+)
