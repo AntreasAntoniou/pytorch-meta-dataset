4b70580a4d144f6adb0f9ef7d47fc010665d856f
AntreasAntoniou <a.antoniou@ed.ac.uk>
2022-04-27 12:46:28 +0100

    Add new experiment scripts for CLIP, rename TALI ones

---
+++ tali_vit_pretrained_imagenetv2_train.sh
@@ -0,0 +1,12 @@
+PYTHONHASHSEED=0 python -m src.train --base_config config/base.yaml \
+                        --method_config config/method/finetune.yaml \
+                        --data_config config/data/variable.yaml \
+                        --opts \
+                        base_source ilsvrc_2012_v2 \
+                        val_source aircraft \
+                        arch modus_prime_tali_viat_pretrained \
+                        debug False \
+                        loader_version pytorch \
+                        load_from_timm True \
+                        num_workers 20 \
+
