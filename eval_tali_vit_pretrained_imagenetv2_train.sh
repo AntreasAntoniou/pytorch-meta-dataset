7da9fc93b8704cf79f31548bbb44f1fd582bf794
Antreas Antoniou <antrikohs@gmail.com>
2022-05-03 22:25:09 +0100

    Optimize imports

--- eval_tali_vit_pretrained_imagenetv2_train.sh
+++ eval_tali_vit_pretrained_imagenetv2_train.sh
@@ -1,26 +1,27 @@
 export architecture=modus_prime_tali_viat_pretrained
 gpus=$1
-for method in config/method/protonet.yaml config/method/finetune.yaml config/method/finetune-with-instance-norm.yaml config/method/simpleshot.yaml config/method/maml.yaml config/method/tim_adm.yaml
-do
-    for dataset in aircraft traffic_sign dtd omniglot mscoco cu_birds
-    do
-      echo "python3 eval.py --architecture $architecture --method $method --dataset $dataset --gpus $gpus"
-      PYTHONHASHSEED=0 python -m src.eval  --base_config config/base.yaml \
-      --method_config $method \
-      --data_config config/data/variable.yaml \
-      --opts \
-       gpus $gpus \
-       base_source ilsvrc_2012_v2 \
-       val_source $dataset \
-       test_source $dataset \
-       arch $architecture \
-       val_episodes 10 \
-       eval_mode test \
-       val_batch_size 1 \
-       loader_version pytorch \
-       load_from_timm True ;\
+for dataset in aircraft traffic_sign dtd omniglot mscoco cu_birds
+  do
+    for method in config/method/protonet.yaml config/method/finetune.yaml config/method/finetune-with-instance-norm.yaml config/method/simpleshot.yaml config/method/maml.yaml config/method/tim_adm.yaml
+      do
 
-    done
+        echo "python3 eval.py --architecture $architecture --method $method --dataset $dataset --gpus $gpus"
+        PYTHONHASHSEED=0 python -m src.eval  --base_config config/base.yaml \
+        --method_config $method \
+        --data_config config/data/variable.yaml \
+        --opts \
+         gpus $gpus \
+         base_source ilsvrc_2012_v2 \
+         val_source $dataset \
+         test_source $dataset \
+         arch $architecture \
+         val_episodes 10 \
+         eval_mode test \
+         val_batch_size 1 \
+         loader_version pytorch \
+         load_from_timm True ;\
+
+      done
 done
 
 
