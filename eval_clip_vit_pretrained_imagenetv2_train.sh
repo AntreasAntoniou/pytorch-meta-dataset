7c5f0e3104608388a93a8e015c710c48ab39263c
Antreas Antoniou <antrikohs@gmail.com>
2022-05-03 14:33:15 +0100

    Add instance norm in eval scripts

--- eval_clip_vit_pretrained_imagenetv2_train.sh
+++ eval_clip_vit_pretrained_imagenetv2_train.sh
@@ -15,7 +15,35 @@ PYTHONHASHSEED=0 python -m src.eval  --base_config config/base.yaml \
 				 load_from_timm True ;\
 
 PYTHONHASHSEED=0 python -m src.eval  --base_config config/base.yaml \
+				--method_config config/method/finetune-with-instance-norm.yaml \
+				--data_config config/data/variable.yaml \
+				--opts \
+				 base_source ilsvrc_2012_v2 \
+				 val_source aircraft \
+				 test_source aircraft \
+				 arch $architecture \
+				 val_episodes 10 \
+				 eval_mode test \
+				 val_batch_size 1 \
+				 loader_version pytorch \
+				 load_from_timm True ;\
+
+PYTHONHASHSEED=0 python -m src.eval  --base_config config/base.yaml \
 				--method_config config/method/finetune.yaml \
+				--data_config config/data/variable.yaml \
+				--opts \
+				 base_source ilsvrc_2012_v2 \
+				 val_source traffic_sign \
+				 test_source traffic_sign \
+				 arch $architecture \
+				 val_episodes 10 \
+				 eval_mode test \
+				 val_batch_size 1 \
+				 loader_version pytorch \
+				 load_from_timm True ;\
+
+PYTHONHASHSEED=0 python -m src.eval  --base_config config/base.yaml \
+				--method_config config/method/finetune-with-instance-norm.yaml \
 				--data_config config/data/variable.yaml \
 				--opts \
 				 base_source ilsvrc_2012_v2 \
@@ -57,7 +85,35 @@ PYTHONHASHSEED=0 python -m src.eval  --base_config config/base.yaml \
 				 load_from_timm True ;\
 
 PYTHONHASHSEED=0 python -m src.eval  --base_config config/base.yaml \
+				--method_config config/method/finetune-with-instance-norm.yaml \
+				--data_config config/data/variable.yaml \
+				--opts \
+				 base_source ilsvrc_2012_v2 \
+				 val_source dtd \
+				 test_source dtd \
+				 arch $architecture \
+				 val_episodes 10 \
+				 eval_mode test \
+				 val_batch_size 1 \
+				 loader_version pytorch \
+				 load_from_timm True ;\
+
+PYTHONHASHSEED=0 python -m src.eval  --base_config config/base.yaml \
 				--method_config config/method/finetune.yaml \
+				--data_config config/data/variable.yaml \
+				--opts \
+				 base_source ilsvrc_2012_v2 \
+				 val_source omniglot \
+				 test_source omniglot \
+				 arch $architecture \
+				 val_episodes 10 \
+				 eval_mode test \
+				 val_batch_size 1 \
+				 loader_version pytorch \
+				 load_from_timm True ;\
+
+PYTHONHASHSEED=0 python -m src.eval  --base_config config/base.yaml \
+				--method_config config/method/finetune-with-instance-norm.yaml \
 				--data_config config/data/variable.yaml \
 				--opts \
 				 base_source ilsvrc_2012_v2 \
@@ -99,7 +155,35 @@ PYTHONHASHSEED=0 python -m src.eval  --base_config config/base.yaml \
 				 load_from_timm True ;\
 
 PYTHONHASHSEED=0 python -m src.eval  --base_config config/base.yaml \
+				--method_config config/method/finetune-with-instance-norm.yaml \
+				--data_config config/data/variable.yaml \
+				--opts \
+				 base_source ilsvrc_2012_v2 \
+				 val_source mscoco \
+				 test_source mscoco \
+				 arch $architecture \
+				 val_episodes 10 \
+				 eval_mode test \
+				 val_batch_size 1 \
+				 loader_version pytorch \
+				 load_from_timm True ;\
+
+PYTHONHASHSEED=0 python -m src.eval  --base_config config/base.yaml \
 				--method_config config/method/finetune.yaml \
+				--data_config config/data/variable.yaml \
+				--opts \
+				 base_source ilsvrc_2012_v2 \
+				 val_source cu_birds \
+				 test_source cu_birds \
+				 arch $architecture \
+				 val_episodes 10 \
+				 eval_mode test \
+				 val_batch_size 1 \
+				 loader_version pytorch \
+				 load_from_timm True ;\
+
+PYTHONHASHSEED=0 python -m src.eval  --base_config config/base.yaml \
+				--method_config config/method/finetune-with-instance-norm.yaml \
 				--data_config config/data/variable.yaml \
 				--opts \
 				 base_source ilsvrc_2012_v2 \
