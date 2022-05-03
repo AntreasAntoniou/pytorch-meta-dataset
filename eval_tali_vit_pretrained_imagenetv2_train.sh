26bce669c865161c4cfdf00f797258d9ae2f7818
Antreas Antoniou <antrikohs@gmail.com>
2022-05-03 15:31:26 +0100

    Introduce for loops in bash scripts

--- eval_tali_vit_pretrained_imagenetv2_train.sh
+++ eval_tali_vit_pretrained_imagenetv2_train.sh
@@ -1,197 +1,24 @@
 export architecture=modus_prime_tali_viat_pretrained
+for method in config/method/protonet.yaml config/method/finetune.yaml config/method/finetune-with-instance-norm.yaml config/method/simpleshot.yaml config/method/maml.yaml config/method/tim_adm.yaml
+do
+    for dataset in aircraft traffic_sign dtd omniglot mscoco cu_birds
+    do
 
-PYTHONHASHSEED=0 python -m src.eval  --base_config config/base.yaml \
-				--method_config config/method/finetune.yaml \
-				--data_config config/data/variable.yaml \
-				--opts \
-				 base_source ilsvrc_2012_v2 \
-				 val_source aircraft \
-				 test_source aircraft \
-				 arch $architecture \
-				 val_episodes 10 \
-				 eval_mode test \
-				 val_batch_size 1 \
-				 loader_version pytorch \
-				 load_from_timm True ;\
+      PYTHONHASHSEED=0 python -m src.eval  --base_config config/base.yaml \
+      --method_config $method \
+      --data_config config/data/variable.yaml \
+      --opts \
+       base_source ilsvrc_2012_v2 \
+       val_source $dataset \
+       test_source $dataset \
+       arch $architecture \
+       val_episodes 10 \
+       eval_mode test \
+       val_batch_size 1 \
+       loader_version pytorch \
+       load_from_timm True ;\
 
-PYTHONHASHSEED=0 python -m src.eval  --base_config config/base.yaml \
-				--method_config config/method/finetune-with-instance-norm.yaml \
-				--data_config config/data/variable.yaml \
-				--opts \
-				 base_source ilsvrc_2012_v2 \
-				 val_source aircraft \
-				 test_source aircraft \
-				 arch $architecture \
-				 val_episodes 10 \
-				 eval_mode test \
-				 val_batch_size 1 \
-				 loader_version pytorch \
-				 load_from_timm True ;\
+    done
+done
 
-PYTHONHASHSEED=0 python -m src.eval  --base_config config/base.yaml \
-				--method_config config/method/finetune.yaml \
-				--data_config config/data/variable.yaml \
-				--opts \
-				 base_source ilsvrc_2012_v2 \
-				 val_source traffic_sign \
-				 test_source traffic_sign \
-				 arch $architecture \
-				 val_episodes 10 \
-				 eval_mode test \
-				 val_batch_size 1 \
-				 loader_version pytorch \
-				 load_from_timm True ;\
 
-PYTHONHASHSEED=0 python -m src.eval  --base_config config/base.yaml \
-				--method_config config/method/finetune-with-instance-norm.yaml \
-				--data_config config/data/variable.yaml \
-				--opts \
-				 base_source ilsvrc_2012_v2 \
-				 val_source traffic_sign \
-				 test_source traffic_sign \
-				 arch $architecture \
-				 val_episodes 10 \
-				 eval_mode test \
-				 val_batch_size 1 \
-				 loader_version pytorch \
-				 load_from_timm True ;\
-
-#PYTHONHASHSEED=0 python -m src.eval  --base_config config/base.yaml \
-#				--method_config config/method/finetune.yaml \
-#				--data_config config/data/variable.yaml \
-#				--opts \
-#				 base_source ilsvrc_2012_v2 \
-#				 val_source quickdraw \
-#				 test_source quickdraw \
-#				 arch $architecture \
-#				 val_episodes 10 \
-#				 eval_mode test \
-#				 val_batch_size 1 \
-#				 loader_version pytorch \
-#				 load_from_timm True ;\
-
-PYTHONHASHSEED=0 python -m src.eval  --base_config config/base.yaml \
-				--method_config config/method/finetune.yaml \
-				--data_config config/data/variable.yaml \
-				--opts \
-				 base_source ilsvrc_2012_v2 \
-				 val_source dtd \
-				 test_source dtd \
-				 arch $architecture \
-				 val_episodes 10 \
-				 eval_mode test \
-				 val_batch_size 1 \
-				 loader_version pytorch \
-				 load_from_timm True ;\
-
-PYTHONHASHSEED=0 python -m src.eval  --base_config config/base.yaml \
-				--method_config config/method/finetune-with-instance-norm.yaml \
-				--data_config config/data/variable.yaml \
-				--opts \
-				 base_source ilsvrc_2012_v2 \
-				 val_source dtd \
-				 test_source dtd \
-				 arch $architecture \
-				 val_episodes 10 \
-				 eval_mode test \
-				 val_batch_size 1 \
-				 loader_version pytorch \
-				 load_from_timm True ;\
-
-PYTHONHASHSEED=0 python -m src.eval  --base_config config/base.yaml \
-				--method_config config/method/finetune.yaml \
-				--data_config config/data/variable.yaml \
-				--opts \
-				 base_source ilsvrc_2012_v2 \
-				 val_source omniglot \
-				 test_source omniglot \
-				 arch $architecture \
-				 val_episodes 10 \
-				 eval_mode test \
-				 val_batch_size 1 \
-				 loader_version pytorch \
-				 load_from_timm True ;\
-
-PYTHONHASHSEED=0 python -m src.eval  --base_config config/base.yaml \
-				--method_config config/method/finetune-with-instance-norm.yaml \
-				--data_config config/data/variable.yaml \
-				--opts \
-				 base_source ilsvrc_2012_v2 \
-				 val_source omniglot \
-				 test_source omniglot \
-				 arch $architecture \
-				 val_episodes 10 \
-				 eval_mode test \
-				 val_batch_size 1 \
-				 loader_version pytorch \
-				 load_from_timm True ;\
-
-#PYTHONHASHSEED=0 python -m src.eval  --base_config config/base.yaml \
-#				--method_config config/method/finetune.yaml \
-#				--data_config config/data/variable.yaml \
-#				--opts \
-#				 base_source ilsvrc_2012_v2 \
-#				 val_source fungi \
-#				 test_source fungi \
-#				 arch $architecture \
-#				 val_episodes 10 \
-#				 eval_mode test \
-#				 val_batch_size 1 \
-#				 loader_version pytorch \
-#				 load_from_timm True ;\
-
-PYTHONHASHSEED=0 python -m src.eval  --base_config config/base.yaml \
-				--method_config config/method/finetune.yaml \
-				--data_config config/data/variable.yaml \
-				--opts \
-				 base_source ilsvrc_2012_v2 \
-				 val_source mscoco \
-				 test_source mscoco \
-				 arch $architecture \
-				 val_episodes 10 \
-				 eval_mode test \
-				 val_batch_size 1 \
-				 loader_version pytorch \
-				 load_from_timm True ;\
-
-PYTHONHASHSEED=0 python -m src.eval  --base_config config/base.yaml \
-				--method_config config/method/finetune-with-instance-norm.yaml \
-				--data_config config/data/variable.yaml \
-				--opts \
-				 base_source ilsvrc_2012_v2 \
-				 val_source mscoco \
-				 test_source mscoco \
-				 arch $architecture \
-				 val_episodes 10 \
-				 eval_mode test \
-				 val_batch_size 1 \
-				 loader_version pytorch \
-				 load_from_timm True ;\
-
-PYTHONHASHSEED=0 python -m src.eval  --base_config config/base.yaml \
-				--method_config config/method/finetune.yaml \
-				--data_config config/data/variable.yaml \
-				--opts \
-				 base_source ilsvrc_2012_v2 \
-				 val_source cu_birds \
-				 test_source cu_birds \
-				 arch $architecture \
-				 val_episodes 10 \
-				 eval_mode test \
-				 val_batch_size 1 \
-				 loader_version pytorch \
-				 load_from_timm True ;\
-
-PYTHONHASHSEED=0 python -m src.eval  --base_config config/base.yaml \
-				--method_config config/method/finetune-with-instance-norm.yaml \
-				--data_config config/data/variable.yaml \
-				--opts \
-				 base_source ilsvrc_2012_v2 \
-				 val_source cu_birds \
-				 test_source cu_birds \
-				 arch $architecture \
-				 val_episodes 10 \
-				 eval_mode test \
-				 val_batch_size 1 \
-				 loader_version pytorch \
-				 load_from_timm True ;\
